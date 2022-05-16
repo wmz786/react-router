95829310a8a19605dd7df7f94137f69fa6f669aa
wmz <you@example.com>
2022-05-16 18:51:41 +0430

    create a modal portal component by reactDOM.createPortal

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = () => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer show modals visible active">
+      <div className="ui raised very padded text container segment">
+        <h1>Modal</h1>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
